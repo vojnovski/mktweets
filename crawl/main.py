ab498410feed10f51afa33a4a5fa9a88a4065314
Viktor Vojnovski <vojnovski@gmail.com>
2013-05-03 21:33:35 +0200

    Added check in db. Stops retrieving older user tweets if one of the current batch is found in the DB.

--- crawl/main.py
+++ crawl/main.py
@@ -38,6 +38,7 @@ def getStatusiRateLimited(korisnik, api, maxid=-1):
 def getMinid(statusi):
     return min([p.id for p in statusi])
 
+
 def langPercentage(statusi, language='mk'):
     n = 0
     for status in statusi:
@@ -47,20 +48,35 @@ def langPercentage(statusi, language='mk'):
         return 0
     return n/float(len(statusi))
 
-def getSiteStatusi(korisnik, api, site=True):
+
+def inDb(statusi, db):
+    if statusi:
+        if statusi[0]:
+            if db.tweets.find({"id": statusi[0].id}).count() == 1:
+                return True
+    return False
+
+
+def getSiteStatusi(korisnik, api, db, site=True):
+    print "Go pocnuvam " + korisnik
     statusi = getStatusiRateLimited(korisnik, api)
     if langPercentage(statusi) < 0.1:
         return [], False
+    if inDb(statusi,db):
+        return [], True            
     print "Vkupno: " + str(len(statusi)) + " statusi od @" + korisnik + ": " + statusi[-1].text.encode('ascii', 'ignore')
     if not site:
         return statusi, True
     while True:
         newstatusi = getStatusiRateLimited(korisnik, api, getMinid(statusi)-1)
+        if inDb(newstatusi, db):
+            return statusi, True
         if newstatusi != []:
             statusi += newstatusi
             print "Vkupno: " + str(len(statusi)) + "; Uste statusi od @" + korisnik + ": " + statusi[-1].text.encode('ascii', 'ignore')
         else:
             return statusi, True
+
 
 def getFriendsRateLimited(korisnik, api):
     time_between_retries = 5
@@ -75,6 +91,7 @@ def getFriendsRateLimited(korisnik, api):
             time_between_retries *= 2
             time.sleep(time_between_retries)
 
+
 def snimi(statuses, db):
     for status in statuses:
         try:
@@ -84,7 +101,7 @@ def snimi(statuses, db):
 
 def svrti(korisnik, iteracija, db, api, tree):
     if korisnik not in projdeni:
-        statuses, mk = getSiteStatusi(korisnik, api,  True)
+        statuses, mk = getSiteStatusi(korisnik, api, db, True )
         snimi(statuses, db)
         projdeni.add(korisnik)
         if mk and not iteracija + 1 == broj_na_iteracii:
@@ -94,9 +111,11 @@ def svrti(korisnik, iteracija, db, api, tree):
     else:
         print "Vekje projden: " + korisnik
 
+
 def test():
     api = prepareApi()
     
+
 def prepareApi():
     try:
         fajl = open("twitterkey")
@@ -106,14 +125,16 @@ def prepareApi():
     keys = fajl.readline().split()
     return twitter.Api(consumer_key=keys[0], consumer_secret=keys[1], access_token_key=keys[2], access_token_secret=keys[3])
 
+
 def main():
     start_time = time.time()
     api = prepareApi()
     client = pymongo.MongoClient("localhost", 27017)
     db = client.tweets
     db.tweets.ensure_index('id', unique = True, dropDups = True)
-    tvitovi = svrti("vojnovski", 0, db, api, "vojnovski")
+    svrti("vojnovski", 0, db, api, "vojnovski")
     print "Time of execution: ", time.time() - start_time, "seconds"
+    
     
 if __name__ == '__main__':
     main()
