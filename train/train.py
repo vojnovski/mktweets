44af3c8fb900bfb213628725e725f1b5d1031530
Viktor Vojnovski <viktor.vojnovski@amadeus.com>
2013-05-06 11:30:53 +0200

    Formatting changes

--- train/train.py
+++ train/train.py
@@ -11,44 +11,45 @@ import time
 import pickle
 import pymongo
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.pipeline import FeatureUnion
 from sklearn.datasets.base import Bunch
 from sklearn.naive_bayes import MultinomialNB
+
 
 def load_tweets(db, urls=True, mentions=True):
     """Load and return the tweets from the DB
 
     """
     n_samples = 24
-    tweets_per_sample = 2000
-
+    tweets_per_sample = 100
     tweets = []
-    screen_names= []
+    screen_names = []
     descriptions = []
     names = []
 
     #Manually tagged the first 24 users
-    target = [1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 ,0 ,0 ,0 ,1 ,0 ,0]
-    
+    target = [1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0]
+
     for user in db.tweets.distinct('user.screen_name')[:n_samples]:
         first_tweet = db.tweets.find({'user.screen_name': user})[:1][0]
-        tweets.append(u"\n".join([tweet['text'] for tweet in db.tweets.find({'user.screen_name': user})[:tweets_per_sample]]))
+        tweets.append(
+            u"\n".join([tweet['text'] for tweet in db.tweets.find({'user.screen_name': user})[:tweets_per_sample]]))
         screen_names.append(first_tweet['user']['screen_name'])
         #descriptions.append(first_tweet['user']['description'])  # Some users seem to have no description
         names.append(first_tweet['user']['name'])
-        #populate target[i]: where from?        
-    
-    return Bunch(tweets=tweets, screen_names=screen_names, descriptions=descriptions, names=names, target=target, 
+        #populate target[i]: where from?
+
+    return Bunch(tweets=tweets, screen_names=screen_names, descriptions=descriptions, names=names, target=target,
                  target_names=['Female', 'Male'],
                  DESCR="Database tweets dump, agreggated over users",
                  )
 
 
 def vectorize_tweets(corpus):
-    tweets_char_vectorizer = CountVectorizer(analyzer='char', ngram_range=(1,5), strip_accents=None, charset_error='strict')
-    tweets_word_vectorizer = CountVectorizer(analyzer='word', ngram_range=(1,3), strip_accents=None, charset_error='strict')
-    tweets_combined_features = FeatureUnion([("tweets_char", tweets_char_vectorizer), ("tweets_word", tweets_word_vectorizer)])  
-    X = tweets_combined_features.fit_transform(corpus.tweets) 
+    tweets_char_vect = CountVectorizer(analyzer='char', ngram_range=(1, 5), strip_accents=None, charset_error='strict')
+    tweets_word_vect = CountVectorizer(analyzer='word', ngram_range=(1, 3), strip_accents=None, charset_error='strict')
+    tweets_combined_features = FeatureUnion([("tweets_char", tweets_char_vect), ("tweets_word", tweets_word_vect)])
+    X = tweets_combined_features.fit_transform(corpus.tweets)
     return tweets_combined_features, X
 
 
@@ -68,7 +69,7 @@ def test_classifier(clf, vectorizer, users, tweets):
     X_new = vectorizer.transform(tweets)
     predicted = clf.predict(X_new)
     for doc, category in zip(users, predicted):
-        print '%r => %s' % (doc, 'Female' if category == 0 else 'Male')   
+        print '%r => %s' % (doc, 'Female' if category == 0 else 'Male')
 
 
 def main():
@@ -77,15 +78,13 @@ def main():
     db = client.tweets
     corpus = load_tweets(db, 'all')
     vectorizer, X = vectorize_tweets(corpus)
-    clf = train_classifier(X,corpus)
-    pickle.dump( (vectorizer, clf), open( "classifier", "wb" ) )
-    #vectorizer, clf = pickle.load(open ("classifier", "rb"))
-    test_users = ['vojnovski']
+    clf = train_classifier(X, corpus)
+    pickle.dump((vectorizer, clf), open("classifier", "wb"))
+    #vectorizer, clf = pickle.load(open("classifier", "rb"))
+    test_users = ['Goran_Man']
     test_classifier(clf, vectorizer, test_users, load_users(db, test_users))
     print "Time of execution: ", time.time() - start_time, "seconds"
-    
-    
+
+
 if __name__ == '__main__':
     main()
-
-
